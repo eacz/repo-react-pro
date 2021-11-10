6f61f0e38e0717dc29ee2af66b9d6caefad1254c
Esteban Canteros <estebanacanteros@gmail.com>
2021-11-10 18:54:47 -0300

    init

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,45 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+import logo from '../logo.svg'
+
+const Navigation = () => {
+  return (
+    <Router>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink activeClassName="nav-active" exact to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="nav-active" exact to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="nav-active" exact to="/users">Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+
+        <Switch>
+          <Route path="/about">
+            <h1>about</h1>
+          </Route>
+          <Route path="/users">
+            <h1>users</h1>
+          </Route>
+          <Route path="/">
+            <h1>home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default Navigation

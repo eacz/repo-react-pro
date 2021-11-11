0da60fa672a2c4baf08b6dc5be80b778bbdde3eb
Esteban Canteros <estebanacanteros@gmail.com>
2021-11-11 08:14:50 -0300

    lazyload pages added

---
+++ src/01-lazyload/pages/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+import LazyPage1 from "./LazyPage1";
+import LazyPage2 from './LazyPage2';
+import LazyPage3 from "./LazyPage3";
+
+export default {
+  LazyPage1, LazyPage2, LazyPage3
+}
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -4,6 +4,9 @@ import {
   Route,
   NavLink
 } from "react-router-dom";
+import LazyPage1 from "../01-lazyload/pages/LazyPage1";
+import LazyPage2 from "../01-lazyload/pages/LazyPage2";
+import LazyPage3 from "../01-lazyload/pages/LazyPage3";
 import logo from '../logo.svg'
 
 const Navigation = () => {
@@ -14,27 +17,27 @@ const Navigation = () => {
           <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink activeClassName="nav-active" exact to="/">Home</NavLink>
+              <NavLink activeClassName="nav-active" exact to="/lazy1">lazy1</NavLink>
             </li>
             <li>
-              <NavLink activeClassName="nav-active" exact to="/about">About</NavLink>
+              <NavLink activeClassName="nav-active" exact to="/lazy2">lazy2</NavLink>
             </li>
             <li>
-              <NavLink activeClassName="nav-active" exact to="/users">Users</NavLink>
+              <NavLink activeClassName="nav-active" exact to="/lazy3">lazy3</NavLink>
             </li>
           </ul>
         </nav>
 
 
         <Switch>
-          <Route path="/about">
-            <h1>about</h1>
+          <Route path="/lazy1">
+            <LazyPage1 />
           </Route>
-          <Route path="/users">
-            <h1>users</h1>
+          <Route path="/lazy2">
+            <LazyPage2 />
           </Route>
-          <Route path="/">
-            <h1>home</h1>
+          <Route path="/lazy3">
+            <LazyPage3 />
           </Route>
         </Switch>
       </div>

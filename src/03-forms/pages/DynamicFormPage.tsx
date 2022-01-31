94cc17a2ca5ac524678d1cf992f758a400e3e3e9
Esteban Canteros <estebanacanteros@gmail.com>
2022-01-30 22:01:39 -0300

    generating inputs based on json

--- src/03-forms/pages/DynamicFormPage.tsx
+++ src/03-forms/pages/DynamicFormPage.tsx
@@ -1,7 +1,25 @@
+import { Form, Formik } from 'formik'
+import { Checkbox, Input, Select } from '../components/Formik'
+import formJson from '../data/custom-form.json'
+
 const DynamicFormPage = () => {
   return (
     <div>
       <h1>Dynamic Forms</h1>
+
+      <Formik
+        initialValues={{name: ''}}
+        onSubmit={values => console.log(values)}
+      >
+        {({  }) => (
+          <Form>
+            {formJson.map(({type, name, placeholder, label}) => {
+              return <Input key={name} type={type as any} name={name} label={label} placeholder={placeholder}  />
+            })}
+            <button type="submit"> Submit </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }

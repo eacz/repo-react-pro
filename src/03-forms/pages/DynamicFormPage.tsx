71f8a09ef4a5874936b4ccb80a40f28e78c714df
Esteban Canteros <estebanacanteros@gmail.com>
2022-01-30 22:29:15 -0300

    creating formik's initialValues dynamically

--- src/03-forms/pages/DynamicFormPage.tsx
+++ src/03-forms/pages/DynamicFormPage.tsx
@@ -2,21 +2,28 @@ import { Form, Formik } from 'formik'
 import { Checkbox, Input, Select } from '../components/Formik'
 import formJson from '../data/custom-form.json'
 
+const initialValues: {[key: string]: any} = {}
+
+for (const input  of formJson) {
+  initialValues[input.name] = input.value
+}
+
 const DynamicFormPage = () => {
   return (
     <div>
       <h1>Dynamic Forms</h1>
 
       <Formik
-        initialValues={{name: ''}}
+        initialValues={initialValues}
         onSubmit={values => console.log(values)}
       >
-        {({  }) => (
-          <Form>
+        {({ handleReset }) => (
+          <Form noValidate >
             {formJson.map(({type, name, placeholder, label}) => {
               return <Input key={name} type={type as any} name={name} label={label} placeholder={placeholder}  />
             })}
             <button type="submit"> Submit </button>
+            <button type="button" onClick={handleReset} > Reset </button>
           </Form>
         )}
       </Formik>
